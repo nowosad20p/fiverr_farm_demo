da0f8b5a30270f1187398077bc367a51ca5ebb8b
nowosad20p <91263440+nowosad20p@users.noreply.github.com>
2024-02-22 23:03:58 +0100

    Map and collision update

--- scripts/Farm.js
+++ scripts/Farm.js
@@ -7,6 +7,7 @@ class Farm extends Phaser.Scene {
     constructor() {
         super("game-scene");
         this.player;
+        this.fences = []; //list of fences for future reference
     }
 
     preload() {
@@ -17,6 +18,9 @@ class Farm extends Phaser.Scene {
         // this.load.image('house', "./Assets/Buildings/house_player.png");
         this.load.atlas("trees","./Assets/Decorations/treesAtlas-default.png","./Assets/Decorations/treesAtlas.json");
         this.load.atlas('decorations','./Assets/Decorations/decorations.png',"./Assets/Decorations/decorations.json");
+        this.load.atlas('bridges','./Assets/Buildings/Bridges/bridges.png',"./Assets/Buildings/Bridges/bridges.json");
+
+        this.load.atlas('fences',"./Assets/Decorations/fences.png",'./Assets/Decorations/fences.json')
         this.load.image('player', "./Assets/Player/NPC5_door_fall.gif");
         this.load.image('buildings','./Assets/Buildings/buildings.png');
         this.load.image('tiles','./Assets/Tiles/tiles.png');
@@ -32,51 +36,113 @@ class Farm extends Phaser.Scene {
     }
 
     create() {
-       
+        //loading tilemaps
         const map = this.make.tilemap({ key: 'tilemap' })
         const tileset = map.addTilesetImage('tiles', 'tiles');
         const trees = map.addTilesetImage('tree_shake', 'trees');
-        const  buildings = map.addTilesetImage('buildings', 'buildings');
-        
+        const buildings = map.addTilesetImage('buildings', 'buildings');
+       
+        //creating ground
         map.createLayer('ground', tileset);
         map.createLayer('farm_spots', tileset);
-    
+        
+        //adding bridges
+        const bridgesLayer = map.getObjectLayer('bridges')['objects'];
+        const bridgesPhysic = this.physics.add.staticGroup();
+        this.bridges = [];
+        bridgesLayer.forEach(object=>{
+            this.bridges.push(bridgesPhysic.create(object.x,object.y,"bridges",object.name))
+        })
+        //adding fences
+        const fencesLayer = map.getObjectLayer('fences')['objects'];
+        const fencesPhysic = this.physics.add.staticGroup();
+        fencesLayer.forEach(object=>{
+            let fence = fencesPhysic.create(object.x,object.y,"fences",object.name);
+            this.fences.push(fence);
+            
+        })
 
-
+        //adding decorations on which player can step on
         const decorationsLayer = map.getObjectLayer('decorations')['objects'];
         const decorationsPhysic = this.physics.add.staticGroup();
         this.decorations = [];
         decorationsLayer.forEach(object => {
-            // Replace each object with a tile
-           if(object.name.split("_")[0]=="tree"&&object.name.split("_")[1]!="stump"){
-            decorations.push(decorationsPhysic.create(object.x+(object.width/2), object.y-(object.height/2), "trees",object.name));
-           }else{
-            decorations.push(decorationsPhysic.create(object.x,object.y-(object.height/2),"decorations",object.name))
-           }
-            // Additional setup for the tile if needed
+           
+        
+            this.decorations.push(decorationsPhysic.create(object.x,object.y-(object.height/2),"decorations",object.name))
+           
+         
         });
+
         
 
-    
+        //spawning player
+        this.player = this.physics.add.sprite(700, 330, 'player');
         
-        this.player = this.physics.add.sprite(500, 200, 'player');
+        
+        //fences collision
+        this.fences.forEach(fence=>{
+            let splitedName = fence.frame.name.split("_");
+            if(splitedName[1]=="fence"){
+                this.physics.add.collider(this.player,fence);
+                if(splitedName[0]=="stone"&&splitedName[2]=="vertical"){
+                    fence.body.setSize(fence.displayWidth/3,fence.displayHeight);
+                    if(splitedName[3]=="right"){
+                        fence.body.setOffset(0,0);
+                    }else{
+                        fence.body.setOffset(fence.displayWidth*2/3,0)
+                    }
+                }
+            }
+        })
+       
+        //spawning decorations player will be behind
+        const frontDecorationsLayer = map.getObjectLayer('front_decorations')['objects'];
+        const frontDecorationsPhysic = this.physics.add.staticGroup();;
+        this.frontDecorations = [];
+        frontDecorationsLayer.forEach(object => {
+        
+           if(object.name.split("_")[0]=="tree"&&object.name.split("_")[1]!="stump"){
+            //setting up trees
+            let tree =frontDecorationsPhysic.create(object.x+(object.width/2), object.y-(object.height/2), "trees",object.name)
+            this.frontDecorations.push(tree);
+            //setting up collision between player and tree stump
+            this.physics.add.collider(this.player, tree);
+            tree.body.setSize(10,6)
+            tree.body.setOffset(object.width/3, object.height*2.5/3);
+
+           }else{
+
+            let decoration = frontDecorationsPhysic.create(object.x,object.y-(object.height/2),"decorations",object.name)
+            if(object.name=="lantern_on_bottom"){
+               this.physics.add.collider(this.player,decoration);
+               decoration.body.setSize(object.width/2,object.height*0.7);
+               decoration.body.setOffset(object.width/4,object.height*0.3);
+            }
+            this.frontDecorations.push(decoration)
+           }
+            
+        });
+
+        //inputs and camera setup
         this.cursor = this.input.keyboard.createCursorKeys();
-        this.player.body.setSize(14, 20);
-        this.player.body.setOffset(3,12);
         this.cameras.main.startFollow(this.player, true, 0.2, 0.2);
         this.cameras.main.setZoom(3,3)
-        const house = this.physics.add.sprite(560, 110, 'player_house');
+        //adding buildings
+        const house = this.physics.add.sprite(720, 270, 'player_house');
         house.setImmovable(true);
         this.physics.add.collider(this.player, house);
-        const market = this.physics.add.sprite(380, 290, 'market');
+        const market = this.physics.add.sprite(520, 460, 'market');
         market.setImmovable(true);
         this.physics.add.collider(this.player, market);
-      
+        this.player.body.setSize(this.player.width,this.player.height*2/3);
+        this.player.body.setOffset(0,this.player.height/3)
         // this.cameras.main.setBounds(0, 0, window.innerWidth*3, window.innerHeight);**
-        this.playerSpeed = 50;
+        //player stats
+        this.playerSpeed = 80;
     }
     update() {
-      
+      //player movement
         const {
             left,
             right,
